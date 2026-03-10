aeedbbcfa520556b8a113138cf70741dd628c9d3
parveen <86622374+parveenkr511@users.noreply.github.com>
2026-03-10 13:17:42 +0530

    feat: Implement ScrollToTop component
    Adds a component that automatically scrolls the page to the top when the route changes. This improves user experience by preventing users from being stuck at the bottom of the page after navigating.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Navbar, Footer } from './components/Layout';
+import ScrollToTop from './components/ScrollToTop';
 import Home from './pages/Home';
 import Listings from './pages/Listings';
 import PropertyDetail from './pages/PropertyDetail';
@@ -10,6 +11,7 @@ import Admin from './pages/Admin';
 export default function App() {
   return (
     <Router>
+      <ScrollToTop />
       <div className="flex flex-col min-h-screen">
         <Navbar />
         <main className="flex-grow">
